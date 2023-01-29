b809016e2c12fac44a2981ba01555f79d4fc463c
shashank9927 <72872634+shashank9927@users.noreply.github.com>
2023-01-29 20:00:01 +0530

    Create Pattern 8.py

---
+++ Step 1/Step 1.2/Pattern 8.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(0,N):
+            for j in range(0,i):
+                print(" ",end="")
+            for j in range(0,2*N-(2*i+1)):
+                print("*",end="")
+            print("")
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
