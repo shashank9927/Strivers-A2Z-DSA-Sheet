365d7104c2019dae3ab115c2b8924ad589e4599b
shashank9927 <72872634+shashank9927@users.noreply.github.com>
2023-02-10 11:48:10 +0530

    Update selection_sort.py

--- Step 2/Step 2.1/selection_sort.py
+++ Step 2/Step 2.1/selection_sort.py
@@ -1,1 +1,35 @@
+#User function Template for python3
 
+class Solution: 
+    def select(self, arr, i):
+        # code here 
+        min_idx=i
+        n=len(arr)
+        for j in range(i+1,n):
+            if arr[j]<arr[min_idx]:
+                min_idx=j
+        return min_idx
+    
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(len(arr)):
+            min_idx=self.select(arr,i)
+            if min_idx!=i:
+                arr[i],arr[min_idx]=arr[min_idx],arr[i]
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
